3627c9794c15cb3c919e6992400d7929f279b69e
lshillman <lshillman@users.noreply.github.com>
2022-07-30 21:31:26 -0700

    add all routes

--- models/index.js
+++ models/index.js
@@ -5,31 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
-Product.belongsTo(Category);
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-
-Category.hasMany(Product);
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
-
 Product.belongsToMany(Tag,{
   through: {
     model: ProductTag,
     unique:false
   },
-  // as: product_tags // may not need this alias?
+  foreignKey: 'product_id'
+  //as: 'products' // may not need this alias?
 });
 
 // Tags belongToMany Products (through ProductTag)
-
 Tag.belongsToMany(Product,{
   through: {
     model: ProductTag,
     unique:false
   },
-  // as: product_tags // may not need this alias?
+  foreignKey: 'tag_id'
+  //as: 'tags' // may not need this alias?
 });
 
 module.exports = {
